e0ef115417d8f6832481c52c8983bb4be1229c35
Anthony Lukach <anthonylukach@gmail.com>
2014-10-14 13:16:49 -0600

    Reformat tests, add test for form validation

--- tests/django_hstore_mixin_tests/tests.py
+++ tests/django_hstore_mixin_tests/tests.py
@@ -3,6 +3,7 @@ import datetime
 import json
 
 from django.core.exceptions import ValidationError
+from django.forms.models import modelform_factory
 from django.test import TestCase
 
 from django_hstore_mixin_tests.models import TestModel
@@ -12,7 +13,7 @@ class TestHstoreMixin(TestCase):
 
     def setUp(self):
         self.now = datetime.datetime.now()
-        self.e = TestModel.objects.create(
+        self.instance = TestModel.objects.create(
             data=dict(
                 int=1,
                 string='foo',
@@ -23,8 +24,9 @@ class TestHstoreMixin(TestCase):
         )
 
     def test_setting(self):
+        """ Ensure data is set/serialized properly """
         self.assertEqual(
-            self.e._data,
+            self.instance._data,
             {
                 'date': json.dumps(self.now.isoformat()),
                 'int': '1',
@@ -34,47 +36,49 @@ class TestHstoreMixin(TestCase):
             }
         )
 
-        self.e.data['int'] = 3
-        self.e.save()
-        self.assertEqual(self.e._data['int'], '3')
+        self.instance.data['int'] = 3
+        self.instance.save()
+        self.assertEqual(self.instance._data['int'], '3')
 
-        self.e.data['list'] = [2, 'three']
-        self.e.save()
-        self.assertEqual(self.e._data['list'], '[2, "three"]')
+        self.instance.data['list'] = [2, 'three']
+        self.instance.save()
+        self.assertEqual(self.instance._data['list'], '[2, "three"]')
 
-        self.e.data['string'] = 'Foo'
-        self.e.save()
-        self.assertEqual(self.e._data['string'], '"Foo"')
+        self.instance.data['string'] = 'Foo'
+        self.instance.save()
+        self.assertEqual(self.instance._data['string'], '"Foo"')
 
-        self.e.data['dict'] = dict(a=2)
-        self.e.save()
-        self.assertEqual(self.e._data['dict'], '{"a": 2}')
+        self.instance.data['dict'] = dict(a=2)
+        self.instance.save()
+        self.assertEqual(self.instance._data['dict'], '{"a": 2}')
 
     def test_getting(self):
+        """ Ensure data is retrieved/deserialized properly """
         self.assertEqual(
-            self.e.data,
+            self.instance.data,
             dict(int=1, string='foo', date=self.now.isoformat(), list=[1, 'two'], dict={'a': 1})
         )
 
-        self.assertEqual(self.e.data.get('int'), 1)
-        self.assertEqual(self.e.data.get('string'), 'foo')
-        self.assertEqual(self.e.data.get('date'), self.now.isoformat())
-        self.assertEqual(self.e.data.get('list'), [1, 'two'])
-        self.assertEqual(self.e.data.get('dict'), {'a': 1})
-        self.assertEqual(self.e.data.get('notThere'), None)
-        self.assertEqual(self.e.data.get('notThere', 'Nope'), 'Nope')
+        self.assertEqual(self.instance.data.get('int'), 1)
+        self.assertEqual(self.instance.data.get('string'), 'foo')
+        self.assertEqual(self.instance.data.get('date'), self.now.isoformat())
+        self.assertEqual(self.instance.data.get('list'), [1, 'two'])
+        self.assertEqual(self.instance.data.get('dict'), {'a': 1})
+        self.assertEqual(self.instance.data.get('notThere'), None)
+        self.assertEqual(self.instance.data.get('notThere', 'Nope'), 'Nope')
 
-        self.assertEqual(self.e.data['int'], 1)
-        self.assertEqual(self.e.data['string'], 'foo')
-        self.assertEqual(self.e.data['date'], self.now.isoformat())
-        self.assertEqual(self.e.data['list'], [1, 'two'])
-        self.assertEqual(self.e.data['dict'], {'a': 1})
+        self.assertEqual(self.instance.data['int'], 1)
+        self.assertEqual(self.instance.data['string'], 'foo')
+        self.assertEqual(self.instance.data['date'], self.now.isoformat())
+        self.assertEqual(self.instance.data['list'], [1, 'two'])
+        self.assertEqual(self.instance.data['dict'], {'a': 1})
         with self.assertRaises(KeyError):
-            self.e.data['notThere']
+            self.instance.data['notThere']
 
     def test_comparison(self):
+        """ Ensure data is compares in a deserialized form """
         self.assertTrue(
-            self.e.data == dict(
+            self.instance.data == dict(
                 int=1,
                 string='foo',
                 date=self.now.isoformat(),
@@ -84,9 +88,29 @@ class TestHstoreMixin(TestCase):
         )
 
     def test_validation(self):
-        self.assertEqual(self.e._data.get('string'), json.dumps('foo'))
-        self.e.clean()
+        """ Assert data validation throws no error on valid JSON. """
+        self.assertEqual(self.instance._data.get('string'), json.dumps('foo'))
+        self.instance.clean()
 
+    def test_validation_failure(self):
+        """ Assert data validation raises exception on invalid JSON. """
         with self.assertRaises(ValidationError):
-            self.e._data['string'] = "not valid JSON"
-            self.e.clean()
+            self.instance._data['string'] = "not valid JSON"
+            self.instance.clean()
+
+    def test_form_validation__full_data(self):
+        """ Ensure model form validates with empty data """
+        form = modelform_factory(TestModel)
+        f = form(data=self.instance.__dict__)
+        f.full_clean()
+        self.assertEqual(f.errors, {})
+
+    def test_form_validation__empty_data(self):
+        """ Ensure model form validates with empty data """
+        self.instance.data = None
+        self.instance.save()
+
+        form = modelform_factory(TestModel)
+        f = form(data=self.instance.__dict__)
+        f.full_clean()
+        self.assertEqual(f.errors, {})
