73957151af793d0f1336349b503433128a926ac3
Anthony Lukach <anthonylukach@gmail.com>
2014-12-30 12:22:00 -0700

    Avoid self.__class__

--- django_hstore_mixin/data_types.py
+++ django_hstore_mixin/data_types.py
@@ -10,25 +10,25 @@ class JsonDict(dict):
     def __init__(self, data, modelInstance, *args, **kwargs):
         self._modelInstance = modelInstance
         data = data or {}  # Avoid data being NoneType
-        super(self.__class__, self).__init__(data, *args, **kwargs)
+        super(JsonDict, self).__init__(data, *args, **kwargs)
 
     def __setitem__(self, k, v):
         # Serialize value being set
-        super(self.__class__, self).__setitem__(k, toJson(v))
+        super(JsonDict, self).__setitem__(k, serializeItem(v))
 
         # You can't use __setitem__ on a @property (ie. "e.data['foo'] = 1"
         # will return e._data as an object which then gets updated, but those
-        # updates won't # make their way back to the db).  As such, we must
+        # updates won't make their way back to the db).  As such, we must
         # update the db field hiding behind the property manually.
         self._modelInstance._data = self.copy()
 
     def __getitem__(self, key):
         # Deserialize on 'get'
-        return json.loads(super(self.__class__, self).__getitem__(key))
+        return json.loads(super(JsonDict, self).__getitem__(key))
 
     def __getattr__(self, key):
         # Deserialize on 'get'
-        return json.loads(super(self.__class__, self).__getattr__(key))
+        return json.loads(super(JsonDict, self).__getattr__(key))
 
     def __repr__(self):
         # Deserialize values for reproduction
@@ -53,7 +53,7 @@ class JsonDict(dict):
         return {k: v for k, v in self.items()} <= other
 
     def get(self, key, default=None):
-        value = super(self.__class__, self).get(key, default)
+        value = super(JsonDict, self).get(key, default)
 
         # Only deserialize if value was pulled from dict
         if value and value != default:
@@ -61,4 +61,4 @@ class JsonDict(dict):
         return value
 
     def items(self):
-        return [(k, json.loads(v)) for k, v in super(self.__class__, self).items()]
+        return [(k, json.loads(v)) for k, v in super(JsonDict, self).items()]
